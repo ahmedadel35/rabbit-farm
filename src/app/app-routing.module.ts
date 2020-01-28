c54f1b8154a581d66a9c9f0c30379e7a29bd4a2f
ahmed adel <abo3adel35@gmail.com>
2020-01-28 04:33:51 +0200

    create notify page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },
+  {
+    path: 'notify',
+    loadChildren: () => import('./notify/notify.module').then( m => m.NotifyPageModule)
   }
 ];
 
