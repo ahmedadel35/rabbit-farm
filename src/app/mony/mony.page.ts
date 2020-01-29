2422b3cb9b3bcb6dc9a148d89498c8722155bb6a
ahmed adel <abo3adel35@gmail.com>
2020-01-29 02:18:33 +0200

    show mony data

--- src/app/mony/mony.page.ts
+++ src/app/mony/mony.page.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from '../services/database.service';
 
 @Component({
-  selector: 'app-mony',
-  templateUrl: './mony.page.html',
-  styleUrls: ['./mony.page.scss'],
+    selector: 'app-mony',
+    templateUrl: './mony.page.html',
+    styleUrls: ['./mony.page.scss']
 })
 export class MonyPage implements OnInit {
+    private data: Array<{}> | false;
+    constructor(private db: DatabaseService) {
+        this.data = this.db.get('mony');
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
