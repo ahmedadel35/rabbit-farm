e17e683fe4d2c4cc6df84c3de38485805ccdbf64
ahmed adel <abo3adel35@gmail.com>
2020-01-29 22:18:22 +0200

    this page will redirect only to other pages

--- src/app/mony/mony.page.ts
+++ src/app/mony/mony.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DatabaseService } from '../services/database.service';
+// import { DatabaseService } from '../services/database.service';
 
 @Component({
     selector: 'app-mony',
@@ -7,59 +7,55 @@ import { DatabaseService } from '../services/database.service';
     styleUrls: ['./mony.page.scss']
 })
 export class MonyPage implements OnInit {
-    data: Array<{}> | false;
-    doneSum = false;
-    constructor(private db: DatabaseService) {
-        this.loadData();
-    }
+    constructor() {}
 
     ngOnInit() {}
 
-    loadData() {
-        this.data = this.db.get('mony');
-        console.log(this.data);
-        this.showSum();
-    }
+    // loadData() {
+    //     this.data = this.db.get('mony');
+    //     console.log(this.data);
+    //     this.showSum();
+    // }
 
-    addToMony(form) {
-        const f = form.value;
-        const d = new Date();
-        const date = d.getDate() + ' يناير ' + d.getFullYear();
+    // addToMony(form) {
+    //     const f = form.value;
+    //     const d = new Date();
+    //     const date = d.getDate() + ' يناير ' + d.getFullYear();
 
-        if (f.value && f.count) {
-            // @ts-ignore
-            this.data.push({
-                value: f.value,
-                count: f.count,
-                notes: f.notes,
-                date
-            });
+    //     if (f.value && f.count) {
+    //         // @ts-ignore
+    //         this.data.push({
+    //             value: f.value,
+    //             count: f.count,
+    //             notes: f.notes,
+    //             date
+    //         });
 
-            // to save the sum: this.data.filter(x => x.sum !== true)
-            this.db.set('mony', this.data);
-            this.doneSum = false;
-        }
-    }
+    //         // to save the sum: this.data.filter(x => x.sum !== true)
+    //         this.db.set('mony', this.data);
+    //         this.doneSum = false;
+    //     }
+    // }
 
-    showSum() {
-        let vc = 0,
-            tc = 0;
-        // @ts-ignore
-        this.data.map(current => {
-            if (!current.sum) {
-                vc += parseFloat(current.value);
-                tc += parseFloat(current.count);
-            }
-        });
-        // @ts-ignore
-        this.data.push({
-            value: vc,
-            count: tc,
-            notes: '=====',
-            date: '25 فبراير 2020',
-            sum: true
-        });
+    // showSum() {
+    //     let vc = 0,
+    //         tc = 0;
+    //     // @ts-ignore
+    //     this.data.map(current => {
+    //         if (!current.sum) {
+    //             vc += parseFloat(current.value);
+    //             tc += parseFloat(current.count);
+    //         }
+    //     });
+    //     // @ts-ignore
+    //     this.data.push({
+    //         value: vc,
+    //         count: tc,
+    //         notes: '=====',
+    //         date: '25 فبراير 2020',
+    //         sum: true
+    //     });
 
-        this.doneSum = true;
-    }
+    //     this.doneSum = true;
+    // }
 }
