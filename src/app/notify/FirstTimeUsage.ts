67fa333093e54879f8e465cc5c118d379ecc059f
ahmed adel <abo3adel35@gmail.com>
2020-01-30 01:26:19 +0200

    redifine the data

--- src/app/notify/FirstTimeUsage.ts
+++ src/app/notify/FirstTimeUsage.ts
@@ -36,7 +36,7 @@ export class FirstTimeUsage {
                 value: 0,
                 info: '',
                 count: 0,
-                date: '25 فبراير 2019',
+                date: '',
                 sum: false
             },
             {
@@ -44,7 +44,7 @@ export class FirstTimeUsage {
                 value: 0,
                 info: '',
                 count: 0,
-                date: '25 فبراير 2019',
+                date: '',
                 sum: false
             },
             {
@@ -52,7 +52,7 @@ export class FirstTimeUsage {
                 value: 0,
                 info: '',
                 count: 0,
-                date: '25 فبراير 2019',
+                date: '',
                 sum: false
             }
         ];
