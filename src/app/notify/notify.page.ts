e9348ac0d386c5e9318f8714c6e3493d270ff9f6
ahmed adel <abo3adel35@gmail.com>
2020-01-30 00:25:09 +0200

    first time uasge

--- src/app/notify/notify.page.ts
+++ src/app/notify/notify.page.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { DatabaseService } from '../services/database.service';
+import { Storage } from '@ionic/storage';
+// import Loader from '../helpers/Loader';
+import FirstTimeUsage from './FirstTimeUsage';
 
 @Component({
-  selector: 'app-notify',
-  templateUrl: './notify.page.html',
-  styleUrls: ['./notify.page.scss'],
+    selector: 'app-notify',
+    templateUrl: './notify.page.html',
+    styleUrls: ['./notify.page.scss']
 })
 export class NotifyPage implements OnInit {
+    constructor(
+        private plt: Platform,
+        private storage: Storage,
+        private db: DatabaseService
+    ) {
+        // this.Loader.showLoader();
 
-  constructor() { }
+        this.plt.ready().then(rbd => {
+            if (rbd) {
+                // check if this first time to use the app
+                this.storage.get('first_time_use').then(fth => {
+                    if (fth) return true; // not the first time
 
-  ngOnInit() {
-  }
+                    // create the basic tables
+                    (new FirstTimeUsage(this.db)).run();
+                });
+            }
+        });
+    }
 
+    ngOnInit() {}
 }
