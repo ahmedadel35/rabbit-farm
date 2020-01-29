eb96dba8d3cee6c1164bb9b4090ceeab40298f84
ahmed adel <abo3adel35@gmail.com>
2020-01-29 23:38:58 +0200

    use ionic storage

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Platform } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
     providedIn: 'root'
 })
 export class DatabaseService {
-    constructor(private plt: Platform, private http: HttpClient) {
+    constructor(
+        private plt: Platform,
+        private storage: Storage,
+        private http: HttpClient
+    ) {
         this.plt.ready().then(rd => {
+
             const mony = this.get('mony');
             if (!mony) {
                 this.http.get('assets/seed/mony.json').subscribe(x => {
@@ -18,27 +24,29 @@ export class DatabaseService {
         });
     }
 
-    get(key: string): Array<{}> | false {
-        const d = localStorage.getItem(key);
-        if (null !== d) {
-            return JSON.parse(d);
-        }
-        return false;
+    async get(key: string) {
+        await this.storage.get(key).then(d => {
+            if (null !== d) {
+                return JSON.parse(d);
+            }
+
+            return false;
+        });
     }
 
     set(key: string, value: any): void {
-        localStorage.setItem(key, JSON.stringify(value));
+        this.storage.set(key, JSON.stringify(value));
     }
 
-    add(key: string, value: any): void | false {
-        const d = this.get(key);
-
-        if (d) {
-            d.push(value);
-            // save again with new data
-            this.set(key, JSON.stringify(d));
-        }
-
-        return false;
+    async add(key: string, value: any): Promise<boolean> {
+        return await this.get(key).then((d: any | null) => {
+            if (d) {
+                d.push(value);
+                // save with new data
+                this.set(key, d);
+                return true;
+            }
+            return false;
+        });
     }
 }
