6968b02059259b2d4e6cbbcdd68dc1137e3a26f5
ahmed adel <abo3adel35@gmail.com>
2020-01-30 05:24:40 +0200

    show income report

--- src/app/mony/mony.page.ts
+++ src/app/mony/mony.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import Loader from '../helpers/Loader';
+import { LoaderService } from '../services/loader.service';
+import { DatabaseService } from '../services/database.service';
+import Funds from '../interfaces/funds';
+import { stat } from 'fs';
 
 @Component({
     selector: 'app-mony',
@@ -7,8 +13,12 @@ import { Router, NavigationExtras } from '@angular/router';
     styleUrls: ['./mony.page.scss']
 })
 export class MonyPage implements OnInit {
-
-    constructor(private router: Router) {}
+    constructor(
+        private router: Router,
+        public alertCtrl: AlertController,
+        private loader: LoaderService,
+        private db: DatabaseService
+    ) {}
 
     ngOnInit() {}
 
@@ -24,5 +34,100 @@ export class MonyPage implements OnInit {
             }
         };
         this.router.navigate(['list'], navExt);
+    }
+
+    showReport() {
+        // this.loader.show();
+
+        // load date from db
+        this.db.get('funds').then(d => {
+            let income = 0,
+                outcome = 0,
+                inVal = '',
+                ratio = 0,
+                state = '',
+                cssClass = '';
+            // (d as Array<Funds>).forEach(x => {
+            //     if (x.type === 'income') {
+            //         income += x.value;
+            //     } else if (x.type === 'outcome') {
+            //         outcome += x.value;
+            //     }
+            // });
+
+            inVal = Math.abs(income - outcome).toFixed(1);
+            // check if outcome is zero
+            if (outcome === 0) {
+                ratio = 0;
+            } else {
+                // @ts-ignore
+                ratio = parseFloat(income / outcome).toFixed(2);
+            }
+
+            // determine state is
+            if (income > outcome) {
+                state = 'ربح';
+                cssClass = 'success';
+            } else {
+                state = 'خسارة';
+                cssClass = 'danger';
+            }
+
+            // show alert with report data
+            this.showAlert(state, cssClass, income, outcome, inVal, ratio);
+        });
+    }
+
+    private showAlert(
+        state: string,
+        cssClass: string,
+        income: number,
+        outcome: number,
+        value: string,
+        ratio: number
+    ) {
+        const alert = this.alertCtrl
+            .create({
+                header: 'تقرير',
+                cssClass: 'fundsRepo',
+                message:
+                    `<ion-list>
+                <ion-list-header class='` +
+                cssClass +
+                `'>
+                <ion-label>` +
+                    state +
+                    `</ion-label>
+                </ion-list-header>
+                <ion-item>
+                <ion-label>الإيرادات</ion-label>
+                <ion-note slot='end' color='primary'>` +
+                    income +
+                    `</ion-note>
+                </ion-item>
+                <ion-item>
+                <ion-label>المصروفات</ion-label>
+                <ion-note slot='end' color='secondary'>` +
+                    outcome +
+                    `</ion-note>
+                </ion-item>
+                <ion-item>
+                <ion-label>قيمة الربح / الخسارة</ion-label>
+                <ion-note slot='end' color='tertiary'>` +
+                    value +
+                    `</ion-note>
+                </ion-item>
+                <ion-item>
+                <ion-label>معدل الربح / الخسارة</ion-label>
+                <ion-note slot='end' color='warning'>` +
+                    ratio +
+                    `</ion-note>
+                </ion-item>
+                </ion-list>`,
+                buttons: ['OK']
+            })
+            .then(a => {
+                a.present();
+            });
     }
 }
