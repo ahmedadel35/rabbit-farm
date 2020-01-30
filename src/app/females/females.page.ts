76b998645a530bcb6bf9843a7bac9e5e1cc5c777
ahmed adel <abo3adel35@gmail.com>
2020-01-30 17:30:28 +0200

    create button to show add female form

--- src/app/females/females.page.ts
+++ src/app/females/females.page.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-females',
-  templateUrl: './females.page.html',
-  styleUrls: ['./females.page.scss'],
+    selector: 'app-females',
+    templateUrl: './females.page.html',
+    styleUrls: ['./females.page.scss']
 })
 export class FemalesPage implements OnInit {
+    constructor() {}
 
-  constructor() { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+    addNewFemale() {
+        console.log('some thing');
+    }
 }
