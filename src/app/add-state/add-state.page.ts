5114674ddfd9017c0492602ee282ec539cb935be
ahmed adel <abo3adel35@gmail.com>
2020-02-01 01:30:32 +0200

    fix back button

--- src/app/add-state/add-state.page.ts
+++ src/app/add-state/add-state.page.ts
@@ -1,27 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
+import { DatabaseService } from '../services/database.service';
+import { LoaderService } from '../services/loader.service';
+import Rabbit from '../interfaces/rabbit';
 
 @Component({
-  selector: 'app-add-state',
-  templateUrl: './add-state.page.html',
-  styleUrls: ['./add-state.page.scss'],
+    selector: 'app-add-state',
+    templateUrl: './add-state.page.html',
+    styleUrls: ['./add-state.page.scss']
 })
 export class AddStatePage implements OnInit {
-  state = 1;
-  positive = true;
-  date: string;
+    state = 1;
+    positive = true;
+    date: string;
+    rabbit: Rabbit;
 
-  constructor() { }
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        private db: DatabaseService,
+        private loader: LoaderService
+    ) {}
 
-  ngOnInit() {
-    this.date = (new Date()).toDateString();
-  }
+    ngOnInit() {
+        // set default date to today
+        this.date = new Date().toDateString();
 
-  setState(v: number) {
-    this.state = v;
-  }
+        const routerData = this.router.getCurrentNavigation().extras;
+        if (!routerData.state || !routerData.state.rb) {
+            this.router.navigate(['females']);
+        } else {
+            // get page name and id from state
+            this.rabbit = routerData.state.rb;
 
-  setPositive(v: number): void {
-    this.positive = !!parseInt(v);
-  }
+            console.log(routerData.state.rb);
+        }
+    }
 
+    goBack() {
+        const d: NavigationExtras = {
+            state: {
+                obj: this.rabbit
+            }
+        };
+
+        this.router.navigate(['show'], d);
+    }
+
+    setState(v: number) {
+        this.state = v;
+    }
+
+    setPositive(v: number): void {
+        this.positive = !!parseInt(v);
+    }
 }
