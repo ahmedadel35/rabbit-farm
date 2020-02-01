f90229f1f5b0fa6a7ddbbc49cd9d96c91c141355
ahmed adel <abo3adel35@gmail.com>
2020-02-01 02:36:12 +0200

    change female state

--- src/app/add-state/add-state.page.ts
+++ src/app/add-state/add-state.page.ts
@@ -3,6 +3,10 @@ import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
 import { DatabaseService } from '../services/database.service';
 import { LoaderService } from '../services/loader.service';
 import Rabbit from '../interfaces/rabbit';
+import { NgForm } from '@angular/forms';
+import State from '../interfaces/state';
+import { createDate } from '../common/rabbit';
+import { ToastController } from '@ionic/angular';
 
 @Component({
     selector: 'app-add-state',
@@ -10,7 +14,7 @@ import Rabbit from '../interfaces/rabbit';
     styleUrls: ['./add-state.page.scss']
 })
 export class AddStatePage implements OnInit {
-    state = 1;
+    state: number | string = '1';
     positive = true;
     date: string;
     rabbit: Rabbit;
@@ -19,7 +23,8 @@ export class AddStatePage implements OnInit {
         private router: Router,
         private route: ActivatedRoute,
         private db: DatabaseService,
-        private loader: LoaderService
+        private loader: LoaderService,
+        public toastCtrl: ToastController
     ) {}
 
     ngOnInit() {
@@ -53,5 +58,67 @@ export class AddStatePage implements OnInit {
 
     setPositive(v: number): void {
         this.positive = !!parseInt(v);
+    }
+
+    save(form: NgForm): void {
+        this.loader.show();
+        const f = form.value;
+
+        if (form.invalid) {
+            this.loader.hide();
+            return;
+        }
+
+        const state: State = {
+            state: parseInt(this.state),
+            num: this.rabbit.num,
+            maleNo: f.maleNo,
+            date: createDate(f.date),
+            positive: this.positive,
+            child: {
+                alive: f.alive,
+                dead: f.dead
+            },
+            notes: f.notes
+        };
+
+        // check if state is talqeh then check for male exsitance
+        if (this.state === '1') {
+            this.db.get('males').then(d => {
+                const found = (d as Rabbit[]).some(x => x.num === f.maleNo);
+
+                if (!found) {
+                    // male not found
+                    this.showFeedback(f.maleNo, 0);
+                    this.loader.hide();
+                    return;
+                }
+
+                this.saveDataToDb(f.maleNo, state);
+            });
+        } else {
+            this.saveDataToDb(f.maleNo, state);
+        }
+    }
+
+    private saveDataToDb(maleNo: number, state: State) {
+        // save new state into database
+        this.db.add('states', state).then(d => {
+            this.loader.hide();
+            this.showFeedback(maleNo, 1, 'success');
+            this.goBack();
+        });
+    }
+
+    private showFeedback(num: number, mess: number, color: string = 'danger') {
+        const messages = ['الذكر رقم ' + num + ' غير موجود', 'تم الحفظ بنجاح'];
+        this.toastCtrl
+            .create({
+                message: messages[mess],
+                duration: 2000,
+                showCloseButton: true,
+                color
+            })
+            .then(ts => ts.present());
     }
 }
