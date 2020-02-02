a47e581dd4067625e0426e7d0a2e063ea0eed817
ahmed adel <abo3adel35@gmail.com>
2020-02-02 08:02:53 +0200

    delete any male

--- src/app/males/males.page.ts
+++ src/app/males/males.page.ts
@@ -4,6 +4,7 @@ import { DatabaseService } from '../services/database.service';
 import { LoaderService } from '../services/loader.service';
 import Rabbit from '../interfaces/rabbit';
 import { goToAddNew, getAgeFromArabic } from '../common/rabbit';
+import Ill from '../interfaces/ill';
 
 @Component({
     selector: 'app-males',
@@ -24,7 +25,7 @@ export class MalesPage implements OnInit {
 
         this.db.get('males').then((d: Array<Rabbit>) => {
             this.data = d;
-            this.oldData = d;
+            this.oldData = [...d];
             this.loader.hide();
         });
     }
@@ -39,14 +40,18 @@ export class MalesPage implements OnInit {
 
     filterData(s: string): void {
         if (!s.length) {
-            this.data = this.oldData;
+            this.data = [...this.oldData];
             return;
         }
 
         // user serched for something
-        this.data = this.oldData.filter((x: Rabbit)=> {
-            return x.num === parseInt(s, 10) || (x.name && x.name.indexOf(s) > -1);
+        const d = this.oldData.filter((x: Rabbit) => {
+            return (
+                x.num === parseInt(s, 10) || (x.name && x.name.indexOf(s) > -1)
+            );
         });
+
+        this.data = [...d];
     }
 
     goToRabbit(obj: Rabbit): void {
@@ -57,5 +62,20 @@ export class MalesPage implements OnInit {
             }
         };
         this.router.navigate(['show'], data);
+    }
+
+    destroy(r: Rabbit, inx: number) {
+        this.loader.show();
+        this.oldData.splice(inx, 1);
+        this.db.set('males', this.oldData);
+
+        // remove from illness table
+        this.db.get('ill').then((i: Ill[]) => {
+            i = i.filter(x => x.num !== r.num);
+            this.db.set('ill', i);
+
+            this.data.splice(inx, 1);
+            this.loader.hide();
+        });
     }
 }
