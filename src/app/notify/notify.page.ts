3daf7d274eb695f7e541c708799c428adb9132a6
ahmed adel <abo3adel35@gmail.com>
2020-02-02 21:06:21 +0200

    show fake notification

--- src/app/notify/notify.page.ts
+++ src/app/notify/notify.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Platform } from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Platform, IonSlides } from '@ionic/angular';
 import { DatabaseService } from '../services/database.service';
 import { LoaderService } from '../services/loader.service';
 import { Storage } from '@ionic/storage';
@@ -11,26 +11,56 @@ import { FirstTimeUsage, FirstTimeKey } from './FirstTimeUsage';
     styleUrls: ['./notify.page.scss']
 })
 export class NotifyPage implements OnInit {
+    initHasPlayed = false;
+    activeSlide = 0;
+    sliderVal = 'talqeh';
+    slideOpts = {
+        speed: 400,
+        centeredSlides: false
+    };
+    slidesArr = ['talqeh', 'gas', 'welada', 'fetam', 'ill'];
+
+    @ViewChild('notifySlides', {static: false}) slides: IonSlides;
+
     constructor(
         private plt: Platform,
         private storage: Storage,
         private db: DatabaseService,
         private loader: LoaderService
     ) {
-        // this.loader.show(); 
+        // this.loader.show();
+        // this.plt.ready().then(rbd => {
+        //     if (rbd) {
+        //         // check if this first time to use the app
+        //         this.storage.get(FirstTimeKey).then(fth => {
+        //             if (fth) return true; // not the first time
+        //             // create the basic tables
+        //             (new FirstTimeUsage(this.db)).run();
+        //         });
+        //     }
+        // });
+    }
 
-        this.plt.ready().then(rbd => {
-            if (rbd) {
-                // check if this first time to use the app
-                this.storage.get(FirstTimeKey).then(fth => {
-                    if (fth) return true; // not the first time
+    ionViewDidEnter() {
+        if (!this.initHasPlayed) this.ngOnInit();
+    }
+    ionViewWillLeave() {
+        this.initHasPlayed = false;
+    }
 
-                    // create the basic tables
-                    (new FirstTimeUsage(this.db)).run();
-                });
-            }
+    ngOnInit() {
+        this.initHasPlayed = true;
+    }
+
+    getIndex(slider: IonSlides) {
+        slider.getActiveIndex().then(v => {
+            console.log(v);
+            this.sliderVal = this.slidesArr[v];
         });
     }
 
-    ngOnInit() {}
+    changeSlide(inx: string) {
+        console.log(inx);
+        this.slides.slideTo(this.slidesArr.indexOf(inx));
+    }
 }
