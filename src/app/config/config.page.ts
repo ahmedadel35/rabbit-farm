2d7354e1ad93fea398238d84cc2504005fa1dc29
ahmed adel <abo3adel35@gmail.com>
2020-02-03 07:49:08 +0200

    config testing loading all data and write it as json

--- src/app/config/config.page.ts
+++ src/app/config/config.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DatabaseService } from '../services/database.service';
 import Config from '../interfaces/Config';
 import { AlertController } from '@ionic/angular';
+import { LoaderService } from '../services/loader.service';
 
 @Component({
     selector: 'app-config',
@@ -13,7 +14,11 @@ export class ConfigPage implements OnInit {
     config: Config = {};
     initHasPlayed = false;
 
-    constructor(private db: DatabaseService, public alertCtrl: AlertController) {}
+    constructor(
+        private db: DatabaseService,
+        public alertCtrl: AlertController,
+        public loader: LoaderService
+    ) {}
 
     ionViewDidEnter() {
         if (!this.initHasPlayed) this.ngOnInit();
@@ -29,30 +34,57 @@ export class ConfigPage implements OnInit {
     }
 
     changeConfig(state: string) {
-        this.alertCtrl.create({
-            header: 'الأيام اللازمة',
-            inputs: [
-                {
-                    type: 'number',
-                    name: 'day',
-                    value: this.config[state]
-                }
-            ],
-            buttons: [
-                {
-                    text: 'إلغاء',
-                    role: 'cancel'
-                },
-                {
-                    text: 'حفظ',
-                    handler: (res) => {
-                        if (!res || !res.day) return false;
-
-                        this.config[state] = parseInt(res.day, 10);
-                        this.db.set('config', this.config);
+        this.alertCtrl
+            .create({
+                header: 'الأيام اللازمة',
+                inputs: [
+                    {
+                        type: 'number',
+                        name: 'day',
+                        value: this.config[state]
                     }
-                }
-            ]
-        }).then(a => a.present());
+                ],
+                buttons: [
+                    {
+                        text: 'إلغاء',
+                        role: 'cancel'
+                    },
+                    {
+                        text: 'حفظ',
+                        handler: res => {
+                            if (!res || !res.day) return false;
+
+                            this.config[state] = parseInt(res.day, 10);
+                            this.db.set('config', this.config);
+                        }
+                    }
+                ]
+            })
+            .then(a => a.present());
+    }
+
+    backup() {
+        this.loadAllData();
+    }
+
+    async loadAllData() {
+        const states = await this.db.get('states');
+        const females = await this.db.get('females');
+        const males = await this.db.get('males');
+        const archive = await this.db.get('archive');
+        const funds = await this.db.get('funds');
+        const ill = await this.db.get('ill');
+
+        const data = {
+            config: this.config,
+            states,
+            females,
+            males,
+            archive,
+            funds,
+            ill
+        };
+
+        // console.log(JSON.stringify(data));
     }
 }
