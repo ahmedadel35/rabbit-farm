f0403e1c8c4b83cb1c57cf277d0558f45c05a616
ahmed adel <abo3adel35@gmail.com>
2020-02-03 07:39:23 +0200

    allow user to change default days between events

--- src/app/config/config.page.ts
+++ src/app/config/config.page.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from '../services/database.service';
+import Config from '../interfaces/Config';
+import { AlertController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-config',
-  templateUrl: './config.page.html',
-  styleUrls: ['./config.page.scss'],
+    selector: 'app-config',
+    templateUrl: './config.page.html',
+    styleUrls: ['./config.page.scss']
 })
 export class ConfigPage implements OnInit {
+    // @ts-ignore
+    config: Config = {};
+    initHasPlayed = false;
 
-  constructor() { }
+    constructor(private db: DatabaseService, public alertCtrl: AlertController) {}
 
-  ngOnInit() {
-  }
+    ionViewDidEnter() {
+        if (!this.initHasPlayed) this.ngOnInit();
+    }
+    ionViewWillLeave() {
+        this.initHasPlayed = false;
+    }
 
+    ngOnInit() {
+        this.db.get('config').then((c: any) => {
+            this.config = c;
+        });
+    }
+
+    changeConfig(state: string) {
+        this.alertCtrl.create({
+            header: 'الأيام اللازمة',
+            inputs: [
+                {
+                    type: 'number',
+                    name: 'day',
+                    value: this.config[state]
+                }
+            ],
+            buttons: [
+                {
+                    text: 'إلغاء',
+                    role: 'cancel'
+                },
+                {
+                    text: 'حفظ',
+                    handler: (res) => {
+                        if (!res || !res.day) return false;
+
+                        this.config[state] = parseInt(res.day, 10);
+                        this.db.set('config', this.config);
+                    }
+                }
+            ]
+        }).then(a => a.present());
+    }
 }
