63ff4e24b759557b59e043a1e99f5af9aa98ff90
ahmed adel <abo3adel35@gmail.com>
2020-02-03 16:28:46 +0200

    remove unused classes

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,28 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Platform } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 
 @Injectable({
     providedIn: 'root'
 })
 export class DatabaseService {
-    constructor(
-        private plt: Platform,
-        private storage: Storage,
-        private http: HttpClient
-    ) {
-        this.plt.ready().then(rd => {
-
-            // const mony = this.get('mony');
-            // if (!mony) {
-            //     this.http.get('assets/seed/mony.json').subscribe(x => {
-            //         console.log(x);
-            //         this.set('mony', x);
-            //     });
-            // }
-        });
-    }
+    constructor(private storage: Storage) {}
 
     async get(key: string): Promise<Array<{}> | boolean> {
         return await this.storage.get(key).then(d => {
