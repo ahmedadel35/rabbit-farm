f47a04f142ece5db49c07fa5e0cbaff99280ad1b
ahmed adel <abo3adel35@gmail.com>
2020-02-09 16:16:20 +0200

    save user edited data

--- src/app/add-new/add-new.page.ts
+++ src/app/add-new/add-new.page.ts
@@ -5,7 +5,7 @@ import { LoaderService } from '../services/loader.service';
 import { NgForm } from '@angular/forms';
 import Rabbit from '../interfaces/rabbit';
 import { ToastController } from '@ionic/angular';
-import { createDate } from '../common/rabbit';
+import { createDate, toEngDate } from '../common/rabbit';
 
 @Component({
     selector: 'app-add-new',
@@ -15,7 +15,9 @@ import { createDate } from '../common/rabbit';
 export class AddNewPage implements OnInit {
     pageId = 'females';
     title = 'إنثى جديدة';
+    rabbit: Rabbit;
     showData = false;
+    isEdit = false;
     name: string;
     num: number;
     type: string;
@@ -46,30 +48,48 @@ export class AddNewPage implements OnInit {
             this.title = this.router.getCurrentNavigation().extras.state.page.title;
             this.pageId = this.router.getCurrentNavigation().extras.state.page.id;
             this.showData = this.router.getCurrentNavigation().extras.state.page.showData;
+            this.isEdit = this.router.getCurrentNavigation().extras.state.page.isEdit;
+            this.rabbit = this.router.getCurrentNavigation().extras.state.page.rb;
+
+            if (this.isEdit) {
+                this.name = this.rabbit.name;
+                this.num = this.rabbit.num;
+                this.type = this.rabbit.type;
+                this.date = new Date(toEngDate(
+                    this.rabbit.date,
+                    true
+                ) as string).toDateString();
+                this.source = this.rabbit.source;
+                this.father = this.rabbit.father;
+                this.mother = this.rabbit.mother;
+                this.box = this.rabbit.box;
+                this.eye = this.rabbit.eye;
+                this.weight = this.rabbit.weight;
+            }
 
             // console.log(this.router.getCurrentNavigation().extras.state.page);
         }
 
-        this.db.get(this.pageId).then(x => {
-            // console.log(x)
-        });
+        // this.db.get(this.pageId).then(x => {
+        //     // console.log(x)
+        // });
     }
 
     save(form: NgForm) {
         this.loader.show();
 
         const f: Rabbit = form.value;
-        const d = new Date(f.date);
+        const d = new Date(f.date || this.date);
         const date = createDate(
-            `${d.getFullYear()} ${d.getMonth()+1} ${d.getDate()}`
+            `${d.getFullYear()} ${d.getMonth() + 1} ${d.getDate()}`
         );
         // console.log(date, f.date);
 
         // create rabbit object
         const obj: Rabbit = {
             name: f.name,
-            num: f.num,
-            type: f.type,
+            num: f.num || this.num,
+            type: f.type || this.type,
             date,
             source: f.source,
             father: f.father,
@@ -78,6 +98,8 @@ export class AddNewPage implements OnInit {
             eye: f.eye,
             weight: f.weight
         };
+
+        console.log(obj);
 
         if (this.pageId === 'males') {
             if (typeof f.mother === 'number') {
@@ -105,38 +127,53 @@ export class AddNewPage implements OnInit {
 
     private saveData(obj: Rabbit, form: NgForm) {
         // before add check if this number exists
-        this.db.get(this.pageId).then(d => {
-            const found = (d as Array<Rabbit>).some(x => x.num === obj.num);
-
-            // console.log(found);
-
-            if (found) {
-                this.showFeedback(obj.num, 1);
-                this.loader.hide();
-                return false;
-            }
-
-            this.db.add(this.pageId, obj).then(d => {
-                this.loader.hide();
-                this.showFeedback(obj.num, 2, 'success');
-                // TODO navigate to this rabbit own page
-                const st: NavigationExtras = {
-                    state: {
-                        obj,
-                        male: (this.pageId === 'males')
+        this.db.get(this.pageId).then((d: Rabbit[]) => {
+            // TODO updata if data was edited
+            if (this.isEdit) {
+                // console.log(d);
+                d = d.map(x => {
+                    if (x.num === obj.num) {
+                        x = obj;
+                        // console.log(x);
                     }
-                };
+                    return x;
+                });
+                // console.log(d);
+                this.db.set(this.pageId, d);
+                this.loader.hide();
+                console.log(obj);
+                setTimeout(() => this.showRabbit(obj, true), 300);
+            } else {
+                const found = d.some(x => x.num === obj.num);
 
-                this.router.navigate(['show'], st);
-            });
+                if (found) {
+                    this.showFeedback(obj.num, 1);
+                    this.loader.hide();
+                    return false;
+                }
+                this.db.add(this.pageId, obj).then(d => {
+                    this.loader.hide();
+                    this.showFeedback(obj.num, 2, 'success');
+                    // TODO navigate to this rabbit own page
+                    this.showRabbit(obj);
+                });
+            }
         });
     }
 
-    private showFeedback(
-        num: number,
-        mess: number,
-        color: string = 'danger'
-    ) {
+    showRabbit(obj: Rabbit, isEdit = false) {
+        const st: NavigationExtras = {
+            state: {
+                obj,
+                isEdit,
+                male: this.pageId === 'males'
+            }
+        };
+
+        this.router.navigate([isEdit ? 'showEdited' : 'show'], st);
+    }
+
+    private showFeedback(num: number, mess: number, color: string = 'danger') {
         const messages = [
             'الإم رقم ' + num + ' غير موجودة',
             'الأرنب رقم ' + num + ' مسجل بالفعل',
