ca65925f1458e875029165c72dd339595273b699
ahmed adel <abo3adel35@gmail.com>
2020-02-10 21:18:58 +0200

    show this fund data

--- src/app/mony/mony.page.ts
+++ src/app/mony/mony.page.ts
@@ -80,95 +80,37 @@ export class MonyPage implements OnInit {
         this.router.navigate(['list']);
     }
 
-    showReport() {
-        this.loader.show();
-
-        // load date from db
-        this.db.get('funds').then(d => {
-            let income = 0,
-                outcome = 0,
-                inVal = '',
-                ratio = 0,
-                state = '',
-                cssClass = '';
-            (d as Array<Funds>).forEach(x => {
-                if (x.type === 'income') {
-                    income += x.value;
-                } else if (x.type === 'outcome') {
-                    outcome += x.value;
-                }
-            });
-
-            inVal = Math.abs(income - outcome).toFixed(1);
-            // check if outcome is zero
-            if (outcome === 0) {
-                ratio = 0;
-            } else {
-                // @ts-ignore
-                ratio = parseFloat(income / outcome).toFixed(2);
-            }
-
-            // determine state is
-            if (income > outcome) {
-                state = 'ربح';
-                cssClass = 'success';
-            } else {
-                state = 'خسارة';
-                cssClass = 'danger';
-            }
-
-            // show alert with report data
-            this.showAlert(state, cssClass, income, outcome, inVal, ratio);
-            this.loader.hide();
-        });
-    }
-
-    private showAlert(
-        state: string,
-        cssClass: string,
-        income: number,
-        outcome: number,
-        value: string,
-        ratio: number
+    private showInfo(
+        fund: Funds
     ) {
         const alert = this.alertCtrl
             .create({
-                header: 'تقرير',
+                header: 'بيان',
                 cssClass: 'fundsRepo',
-                message:
-                    `<ion-list>
-                <ion-list-header class='` +
-                    cssClass +
-                    `'>
-                <ion-label>` +
-                    state +
-                    `</ion-label>
-                </ion-list-header>
-                <ion-item>
-                <ion-label>الإيرادات</ion-label>
-                <ion-note slot='end' color='primary'>` +
-                    income +
-                    `</ion-note>
-                </ion-item>
-                <ion-item>
-                <ion-label>المصروفات</ion-label>
-                <ion-note slot='end' color='secondary'>` +
-                    outcome +
-                    `</ion-note>
-                </ion-item>
-                <ion-item>
-                <ion-label>قيمة الربح / الخسارة</ion-label>
-                <ion-note slot='end' color='tertiary'>` +
-                    value +
-                    `</ion-note>
-                </ion-item>
-                <ion-item>
-                <ion-label>معدل الربح / الخسارة</ion-label>
-                <ion-note slot='end' color='warning'>` +
-                    ratio +
-                    `</ion-note>
-                </ion-item>
-                </ion-list>`,
+                message:`
+                <ion-list>
+                    <ion-item>
+                    <ion-note slot='start'>شراء</ion-note>
+                    <ion-label>${fund.type}</ion-label>
+                    </ion-item>
+                    <ion-item>
+                    <ion-note slot='start'>إسم البائع</ion-note>
+                    <ion-label>${fund.seller}</ion-label>
+                    </ion-item>
+                    <ion-item>
+                    <ion-note slot='start'>العدد</ion-note>
+                    <ion-label>${fund.count}</ion-label>
+                    </ion-item>
+                    <ion-item>
+                    <ion-note slot='start'>الوزن</ion-note>
+                    <ion-label>${fund.weight}</ion-label>
+                    </ion-item>
+                    <ion-item>
+                    <ion-note slot='start'>الملاحظات</ion-note>
+                    <ion-label>${fund.info}</ion-label>
+                    </ion-item>
+                </ion-list>
+                `,
                 buttons: ['OK']
             })
             .then(a => {
