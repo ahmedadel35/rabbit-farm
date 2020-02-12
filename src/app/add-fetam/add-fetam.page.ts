f398c59c5ddc5febf22a2e65f61ba17aa9d6b210
ahmed adel <abo3adel35@gmail.com>
2020-02-12 19:08:31 +0200

    save fetam to db

--- src/app/add-fetam/add-fetam.page.ts
+++ src/app/add-fetam/add-fetam.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { createDate } from '../common/rabbit';
+import Fetam from '../interfaces/fetam';
+import { DatabaseService } from '../services/database.service';
+import { LoaderService } from '../services/loader.service';
 
 @Component({
     selector: 'app-add-fetam',
@@ -14,7 +19,7 @@ export class AddFetamPage implements OnInit {
     age: number = null;
     date: string;
 
-    constructor(private router: Router) {}
+    constructor(private router: Router, private db: DatabaseService, public loader: LoaderService) {}
 
     ngOnInit() {
         this.date = new Date().toDateString();
@@ -22,5 +27,31 @@ export class AddFetamPage implements OnInit {
 
     goBack() {
         this.router.navigate(['fetam']);
+    }
+
+    add(form: NgForm) {
+        this.loader.show();
+
+        const f: Fetam = form.value;
+        const d = new Date(f.date || this.date);
+        const date = createDate(
+            `${d.getFullYear()} ${d.getMonth() + 1} ${d.getDate()}`
+        );
+
+        if (form.valid) {
+            const obj: Fetam = {
+                patchNo: f.patchNo,
+                count: f.count,
+                weight: f.weight,
+                age: f.age,
+                date
+            };
+
+            // console.log(obj);
+            this.db.add('fetam', obj).then(d => {
+                this.loader.hide();
+                this.goBack();
+            })
+        }
     }
 }
