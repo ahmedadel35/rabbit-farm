4fe5c7edbc152c2313e2945a80547f1c15c3e70e
ahmed adel <abo3adel35@gmail.com>
2020-02-12 18:14:59 +0200

    log all data in fetam page

--- src/app/fetam/fetam.page.ts
+++ src/app/fetam/fetam.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import Fetam from '../interfaces/fetam';
+import { DatabaseService } from '../services/database.service';
 
 @Component({
-  selector: 'app-fetam',
-  templateUrl: './fetam.page.html',
-  styleUrls: ['./fetam.page.scss'],
+    selector: 'app-fetam',
+    templateUrl: './fetam.page.html',
+    styleUrls: ['./fetam.page.scss']
 })
 export class FetamPage implements OnInit {
+    data: Fetam[] = [];
+    constructor(private db: DatabaseService) {}
 
-  constructor() { }
+    ngOnInit() {
+        this.db.get('fetam').then((d: Fetam[]) => {
+            // console.log(d);
+            d = d.filter(x => x.date !== 'noDate');
 
-  ngOnInit() {
-  }
-
+            this.data = d;
+        });
+    }
 }
