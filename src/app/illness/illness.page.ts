03c9626a4c6c770cc840ed0d7975621f1b3f89db
ahmed adel <abo3adel35@gmail.com>
2020-02-12 17:33:05 +0200

    show list of illness and its vaccines

--- src/app/illness/illness.page.ts
+++ src/app/illness/illness.page.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-illness',
-  templateUrl: './illness.page.html',
-  styleUrls: ['./illness.page.scss'],
+    selector: 'app-illness',
+    templateUrl: './illness.page.html',
+    styleUrls: ['./illness.page.scss']
 })
 export class IllnessPage implements OnInit {
+    data: Array<{name: string, vac: string}> = [];
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.data = [
+            {
+                name: 'جرب',
+                vac: 'علاج الجرب'
+            },
+            {
+                name: 'سل',
+                vac: 'علاج السل'
+            },
+        ];
+    }
 }
