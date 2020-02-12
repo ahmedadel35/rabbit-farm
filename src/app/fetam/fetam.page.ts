678ac0b8e9cb91a8ab170b0e7d387f6e4fa7f3eb
ahmed adel <abo3adel35@gmail.com>
2020-02-12 19:10:48 +0200

    load all data in fetam page

--- src/app/fetam/fetam.page.ts
+++ src/app/fetam/fetam.page.ts
@@ -8,15 +8,26 @@ import { DatabaseService } from '../services/database.service';
     styleUrls: ['./fetam.page.scss']
 })
 export class FetamPage implements OnInit {
+    initHasPlayed = false;
     data: Fetam[] = [];
+
     constructor(private db: DatabaseService) {}
 
+    ionViewDidEnter() {
+        if (!this.initHasPlayed) this.ngOnInit();
+    }
+    ionViewWillLeave() {
+        this.initHasPlayed = false;
+    }
+
     ngOnInit() {
+        this.initHasPlayed = true;
         this.db.get('fetam').then((d: Fetam[]) => {
             // console.log(d);
             d = d.filter(x => x.date !== 'noDate');
 
             this.data = d;
+            console.log(d);
         });
     }
 }
