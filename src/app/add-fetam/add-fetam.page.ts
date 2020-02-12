5a29359a0e96762ca07afb3bb6d5b425d01d66ef
ahmed adel <abo3adel35@gmail.com>
2020-02-12 18:57:12 +0200

    run back button

--- src/app/add-fetam/add-fetam.page.ts
+++ src/app/add-fetam/add-fetam.page.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-add-fetam',
-  templateUrl: './add-fetam.page.html',
-  styleUrls: ['./add-fetam.page.scss'],
+    selector: 'app-add-fetam',
+    templateUrl: './add-fetam.page.html',
+    styleUrls: ['./add-fetam.page.scss']
 })
 export class AddFetamPage implements OnInit {
-  // form inputs
-  patchNo?: number = null;
-  count: number = null;
-  weight: number = null;;
-  age: number = null;;
-  date: string;
+    // form inputs
+    patchNo?: number = null;
+    count: number = null;
+    weight: number = null;
+    age: number = null;
+    date: string;
 
-  constructor() { }
+    constructor(private router: Router) {}
 
-  ngOnInit() {
-    this.date = (new Date()).toDateString()
-  }
+    ngOnInit() {
+        this.date = new Date().toDateString();
+    }
 
+    goBack() {
+        this.router.navigate(['fetam']);
+    }
 }
