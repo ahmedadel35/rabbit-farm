ab2c9062c60e8af4992159aaad2d2c66b9b71039
ahmed adel <abo3adel35@gmail.com>
2020-02-22 11:50:12 +0200

    show events assioated to one date

--- src/app/calender/calender.page.ts
+++ src/app/calender/calender.page.ts
@@ -4,6 +4,7 @@ import { AlertController } from '@ionic/angular';
 import { LoaderService } from '../services/loader.service';
 import State from '../interfaces/state';
 import { toEngDate, createDate } from '../common/rabbit';
+import { type } from 'os';
 
 @Component({
     selector: 'app-calender',
@@ -12,6 +13,7 @@ import { toEngDate, createDate } from '../common/rabbit';
 })
 export class CalenderPage implements OnInit {
     public data: State[] = [];
+    public days: State[] = [];
     public statesStr = ['', 'تلقيح', 'جس', 'ولادة'];
 
     constructor(
@@ -29,10 +31,19 @@ export class CalenderPage implements OnInit {
         let states = (await this.db.get('states')) as State[];
 
         // TODO load only states with upcoming date
-        states = states.filter(x => x.date !== 'noDate' && x.state !== 4);
-        this.data = states;
+        states.map(x => {
+            if (x.date !== 'noDate' && x.state !== 4) {
+                x.date = this.turnTODate(x.date);
+                if (!this.days[0] || this.days[this.days.length - 1].date !== x.date) {
+                    this.days.push(x);
+                }
+                this.data.push(x);
+            }
+        });
+        console.log(this.days);
+        // this.data = states;
 
-        console.log(states);
+        console.log(this.data);
 
         this.loader.hide();
     }
@@ -41,7 +52,40 @@ export class CalenderPage implements OnInit {
         return createDate(
             toEngDate(d, true) as string,
             undefined,
-            'dddd DD MMMM(MM) YYYY'
+            'dddd DD MMMM(M) YYYY'
         );
+    }
+
+    show(s: State) {
+        const arr = this.data.filter(x => x.date === s.date);
+        console.log(arr);
+        // if (arr.length)
+        this.alertCreate(s.date, arr);
+    }
+
+    async alertCreate(date: string, arr: State[]) {
+        let message = '<ion-list>';
+        arr.forEach((x, inx) => {
+            message += `<ion-item>
+            <ion-label class='${inx%2===0 ? "bg-tertiary" : "bg-primary"}'>
+            ${this.statesStr[x.state]}
+            </ion-label>
+            <ion-label class='${inx%2===0 ? "bg-tertiary" : "bg-primary"}'>
+                <ion-icon name='female'></ion-icon>${x.num}
+            </ion-label>
+            </ion-item>`;
+        });
+        message += '</ion-list>';
+        const al = await this.alertCtrl.create({
+            header: date,
+            cssClass: 'fundsRepo calendar',
+            message,
+            buttons: [{
+                role: 'cancel',
+                text: 'تم'
+            }]
+        });
+
+        al.present();
     }
 }
