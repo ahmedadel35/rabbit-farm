5e51846ae83eb95da1d19529a1f9fa47698a166b
ahmed adel <abo3adel35@gmail.com>
2020-03-12 19:27:49 +0200

    hide loader after 500 milleseconds

--- src/app/services/loader.service.ts
+++ src/app/services/loader.service.ts
@@ -16,17 +16,6 @@ export class LoaderService {
      * @memberof Loader
      */
     show() {
-        // this.loaderCtrl
-        //     .create({
-        //         backdropDismiss: false,
-        //         message: 'يرجى الإنتظار',
-        //         duration: 7000
-        //     })
-        //     .then(l => {
-        //         this.loader = l;
-        //         this.loader.present().then(p => p);
-        //     });
-
         this.loaderCtrl.show();
     }
 
@@ -37,17 +26,6 @@ export class LoaderService {
      */
     hide() {
         this.loaderCtrl.hide();
-        // if (this.loader) {
-        //     this.hideLoader(700);
-        // } else {
-        //     this.hideLoader(2500);
-        // }
-    }
-
-    private hideLoader(timeout: number): void {
-        setTimeout(x => {
-            if (this.loader) this.loader.dismiss().then(d => d);
-            this.loader = null;
-        }, timeout);
+        setTimeout(x => this.loaderCtrl.hide(), 500);
     }
 }
